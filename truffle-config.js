c85850648e39689123dd7873b0896e6518a8d99f
Luke <luke@Lukes-Air.attlocal.net>
2021-09-23 23:45:25 -0500

    updated App.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,20 @@
 //require('babel-register');
 //require('babel-polyfill');
-//require('dotenv').config();
-//const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 
 
 module.exports = {
   networks: {
-    /*rinkeby: {
+    rinkeby: {
       provider: function() {
         return new HDWalletProvider(process.env.MNEMONIC,
         'https://rinkeby.infura.io/v3/aa04761cc9f44beabd25dd6cbe23abf9')
       },
       //gasPrice: 25000000000,
       network_id: 4
-    },*/
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
@@ -22,7 +22,7 @@ module.exports = {
     },
   },
   //contracts_directory: './src/contracts/',
-  //contracts_build_directory: './src/abis/',
+  contracts_build_directory: './my-app/src/abis/',
   compilers: {
     solc: {
       version: "0.8.0",
